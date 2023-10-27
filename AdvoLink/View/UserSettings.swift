d38e7a42c35a8b6e877d3ba4c5481749c26e0b99
thejerryw <jerrywang@Jerrys-MacBook-Air.local>
2023-10-27 16:57:57 -0700

    added user settings, not complete w images though

---
+++ AdvoLink/View/UserSettings.swift
@@ -0,0 +1,18 @@
+//
+//  UserSettings.swift
+//  AdvoLink
+//
+//  Created by Jerry Wang on 10/27/23.
+//
+
+import SwiftUI
+
+struct UserSettings: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserSettings()
+}
